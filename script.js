33d8fa7c15eef411dadf4504dd153047bd111c93
Thom <thomswann@gmail.com>
2022-06-04 19:09:29 -0400

    updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function drawUI() {
   const script = document.createElement("script");
   script.type = "text/javascript";
-  script.innerText = `
+  script.innerHTML = `
     $(document).ready(function(){ 
       function getBalance() {
         return parseFloat($("#balance").html());
@@ -27,8 +27,8 @@ function drawUI() {
       }
 
       function playGame()  {
-        const rand = Math.random();
-        if (rand >= .42) {
+        const rand = Math.floor(Math.random() * 100000);
+        if (rand <= 42000 || rand >= 84000) {
           document.getElementById("double_your_btc_bet_hi_button").click();
         } else {
           document.getElementById("double_your_btc_bet_lo_button").click();
@@ -46,9 +46,9 @@ function drawUI() {
         balance = getBalance();
         
         document.getElementById("amount-2").innerText = amounts[1];
-        document.getElementById("winning-2").innerText = parseFloat(amounts[1] * .5).toFixed(8);
+        document.getElementById("winning-2").innerText = parseFloat(amounts[1] * .1).toFixed(8);
         document.getElementById("amount-3").innerText = amounts[2];
-        document.getElementById("winning-3").innerText = parseFloat(amounts[2] * .5).toFixed(8);
+        document.getElementById("winning-3").innerText = parseFloat(amounts[2] * .1).toFixed(8);
         document.getElementById("game-wins").innerText = wins;
         document.getElementById("game-loss").innerText = loss;
         document.getElementById("game-total").innerText = wins + loss;
@@ -58,22 +58,10 @@ function drawUI() {
       }
 
       function loopGame(streak, maxStreak, resetStreak) {
-        if(window && window.PLAY_MULTIPLY_BTC){
-          currentRatio = 1.50;
-          setRatio("1.50");
-          let override = false;
-          const checkG = checkGame();
-          if (checkG.won) {
-            wins++;
-            winBTC += currentBetAmount * (currentRatio - 1);
-          } else if (checkG.lost) {
-            loss++;
-            lostBTC += parseFloat(currentBetAmount);
-            override = true;
-            currentBetAmount = amounts[2];
-            setBetAmount(amounts[2]);
-            playGame();
-          }
+        if (window && window.PLAY_MULTIPLY_BTC) {
+          
+          currentRatio = 1.10;
+          setRatio("1.10");
       
           if (!override) {
             if (streak < maxStreak) {
@@ -89,15 +77,32 @@ function drawUI() {
             } else {
               streak = 0;
             }
+          } else {
+            override = false;
+            currentBetAmount = amounts[2];
+            setBetAmount(amounts[2]);
+            playGame();
           }
         } 
-        setTimeout(function() { 
+        setTimeout(() => { 
+          if (streak !== 0) {
+            const checkG = checkGame();
+            if (checkG.won) {
+              wins++;
+              winBTC += currentBetAmount * (currentRatio - 1);
+            } else if (checkG.lost) {
+              loss++;
+              lostBTC += parseFloat(currentBetAmount);
+              override = true;
+            }
+          }
           updateText();
           loopGame(streak, maxStreak, resetStreak); 
         }, streak === 0 || !window.PLAY_MULTIPLY_BTC ? 100 : 5000);
       }
       
-      loopGame(0, 5, 10);
+      loopGame(0, 4, 2);
+      
       document.getElementById("test-btn").addEventListener(
         "click",
         function() {
@@ -112,6 +117,81 @@ function drawUI() {
           }
         }
       );
+
+      document.getElementById("AUTO-PLAY-BTN").addEventListener(
+        "click",
+        function () {
+          window.AUTOPLAY_MULTIPLY_BTC = !window.AUTOPLAY_MULTIPLY_BTC;
+          if (window.AUTOPLAY_MULTIPLY_BTC) {
+            document.getElementById("AUTO-PLAY-BTN").innerText = "AutoPlay (ON)";
+          } else {
+            document.getElementById("AUTO-PLAY-BTN").innerText = "AutoPlay (OFF)";
+          }
+        }
+      );
+      
+      function clickRoll() {
+        window.scrollTo({ top: 1000, behavior: 'smooth' });
+        const timing = Math.floor(Math.random() * 10) * 1000;
+        console.log("Rolling in " + timing / 1000 + "s");
+        setTimeout(function () {
+          var button = document.getElementById("free_play_form_button");
+          if (button.style.display !== 'none') {
+            try {
+              button.click();
+            } catch (e) { }
+            console.log("Rolled");
+            setTimeout(function () {
+              closeModal();
+              if (window.AUTOPLAY_MULTIPLY_BTC) {
+                $('.test-btn').click();
+              }
+            }, 5000);
+          } else {
+            console.log('No Roll');
+            if (window.AUTOPLAY_MULTIPLY_BTC) {
+              $('.test-btn').click();
+            }
+          }
+      
+        }, timing);
+      }
+
+      function closeModal() {
+        try {
+          document.getElementsByClassName('pushpad_deny_button')[0].click();
+        } catch (e) { }
+        var c = document.getElementsByClassName('reveal-modal');
+        Array.from(c).map(function (d) {
+          if (d.classList.contains('open')) {
+            const e = d.getElementsByClassName('close-reveal-modal')[0];
+            console.log('Modal closed');
+            try { e.click(); } catch (e) { }
+          }
+        })
+        const elements = document.getElementsByClassName('reveal-modal');
+        let elementsI = elements.length;
+        while (elementsI--) {
+          elements[elementsI].style.display = 'none';
+        }
+        clickRoll()
+      }
+      function start() {
+        const baseTiming = Math.floor(((Math.random() * 30) + 1) * 1000);
+        console.log("Cleaning in " + baseTiming / 1000 + "s");
+        setTimeout(function () {
+          closeModal();
+        }, baseTiming);
+      }
+      
+      start();
+
+      if (window.AUTOPLAY_MULTIPLY_BTC) {
+        document.getElementById("AUTO-PLAY-BTN").innerText = "AutoPlay (ON)";
+      } else {
+        document.getElementById("AUTO-PLAY-BTN").innerText = "AutoPlay (OFF)";
+      }
+
     }); 
   `
 
@@ -128,7 +208,8 @@ function drawUI() {
     <div class="MY-PANEL" style="background-color:white;border-radius:6px;width:350px;overflow-y:hidden;opacity:0.5;">
     <div style="text-align:center;padding:12px;margin-bottom:6px;background-color:#008F8C;"><h5 style="color:white;margin:0px;">🚀 BTC</h5></div>
     <div style="padding:5px 12px 8px 12px;display:flex;justify-content:center;">
-      <button id="test-btn" style="font-size:12px;background-color:#015958;padding:6px 16px; border:0px;border-radius:7px;">Start MultiplyBTC</button>
+      <button id="test-btn" style="font-size:12px;background-color:#015958;padding:6px 16px; border:0px;border-radius:7px;margin-right:8px">Start MultiplyBTC</button>
+      <button id="AUTO-PLAY-BTN" style="font-size:12px;background-color:#015958;padding:6px 16px; border:0px;border-radius:7px;"></button>
     </div>
     <div style="padding:0px 12px;">
       <table style="width:100%;">
@@ -181,48 +262,5 @@ function drawUI() {
   }, 500);
 }
 drawUI();
-function clickRoll() {
-  window.scrollTo({ top: 1000, behavior: 'smooth' })
-  const timing = Math.floor(Math.random() * 10) * 1000;
-  console.log(`Rolling in ${timing / 1000}s`);
-  setTimeout(function () {
-    var button = document.getElementById("free_play_form_button");
-    if (button.style.display !== 'none') {
-      try {
-        button.click()
-      } catch (e) { }
-      console.log(`Rolled`)
-      setTimeout(function () { closeModal(); }, 5000);
-    } else {
-      console.log('No Roll')
-    }
-  }, timing);
-}
-function closeModal() {
-  try {
-    document.getElementsByClassName('pushpad_deny_button')[0].click();
-  } catch (e) { }
-  var c = document.getElementsByClassName('reveal-modal')
-  Array.from(c).map(d => {
-    if (d.classList.contains('open')) {
-      const e = d.getElementsByClassName('close-reveal-modal')[0]
-      console.log('Modal closed')
-      try { e.click(); } catch (e) { }
-    }
-  })
-  const elements = document.getElementsByClassName('reveal-modal');
-  let elementsI = elements.length;
-  while (elementsI--) {
-    elements[elementsI].style.display = 'none';
-  }
-  clickRoll()
-}
-function start() {
-  const baseTiming = Math.floor(((Math.random() * 30) + 1) * 1000);
-  console.log(`Cleaning in ${baseTiming / 1000}s`)
-  setTimeout(function () {
-    closeModal();
-  }, baseTiming);
-}
 
-start();
+
