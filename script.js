edb1a1d0fe65399e843172bde6b4475b924f96de
Thom <thomswann@gmail.com>
2022-06-06 17:15:32 -0400

    asdasd

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function drawUI() {
           document.getElementById("double_your_btc_bet_lose").style.display = 'none';
           if (result.won) {
             wins++;
-            winBTC += document.getElementsByClassName('double_your_btc_bet_win_message')[0].innerText.replace('You BET HI so you win ', '').replace(' BTC', '');
+            winBTC += document.getElementsByClassName('double_your_btc_bet_win_message')[0].innerText.replace('You BET HI so you win ', '').replace(' BTC!', '');
             const startingBetAmount = (parseInt(document.getElementById("PLAY-STARTING-BET").value) / 100000000).toFixed(8);
             currentBetAmount = parseFloat(startingBetAmount).toFixed(8);
             setBetAmount(startingBetAmount);
